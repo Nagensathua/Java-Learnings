aed42a7043fae4d25407632991976f9a03b2ba4b
Nagensathua <nagenkumar94@gmail.com>
2025-03-20 02:53:02 +0530

    exception handling notes

--- src/main/java/com/learn/ExceptionHandling/ExceptionHandlingProgram.java
+++ src/main/java/com/learn/ExceptionHandling/ExceptionHandlingProgram.java
@@ -1,5 +1,21 @@
 package com.learn.ExceptionHandling;
 
 public class ExceptionHandlingProgram {
-
+// Java exception handling is a mechanism to handle runtime error
+	// Types of exception
+	/*
+	 * 1. Unchecked exception - compile time exception
+	 * 2. Checked exception - Runtime Exception
+	 * 3. error - OutOfMemory error, StackOverFlow error 
+	 */
+	// Exception handling keyword
+	/*
+	 * try-catch
+	 * finally 
+	 * throw
+	 * throws
+	 * custom exception
+	 */
+	
+	
 }
